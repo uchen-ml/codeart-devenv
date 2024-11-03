d9c64acb337c36901a10dafd876154e2f90ee9ed
Eugene Ostroukhov <eostroukhov@gmail.com>
2024-11-02 21:22:56 -0700

    Checkpoint

--- src/discovery/compilers.ts
+++ src/discovery/compilers.ts
@@ -1,7 +1,27 @@
 import { spawn } from "node:child_process";
 import { promises as fs } from "node:fs";
 
-const compilers = ["c++", "gcc", "clang", "g++", "clang++"];
+enum CompilerFamily {
+    Unknown = "UNKNOWN",
+    GCC = "GCC",
+    Clang = "Clang",
+}
+
+const CompilerDefaults = [
+    { executable: "c++", family: CompilerFamily.Unknown },
+    { executable: "gcc", family: CompilerFamily.GCC },
+    { executable: "clang", family: CompilerFamily.Clang },
+    { executable: "g++", family: CompilerFamily.GCC },
+    { executable: "clang++", family: CompilerFamily.Clang },
+];
+
+type CompilerDefaultType = (typeof CompilerDefaults)[number];
+
+const CompileVersionArgs = {
+    [CompilerFamily.GCC]: ["--version"],
+    [CompilerFamily.Clang]: ["--version"],
+    [CompilerFamily.Unknown]: ["--version"],
+};
 
 function spawn_process([executable, ...args]: string[]) {
     return new Promise<{ code: number; output: string }>((resolve) => {
@@ -22,32 +42,37 @@ function spawn_process([executable, ...args]: string[]) {
 
 export default class Compilers {
     list = async () => {
-        const resolved = await Promise.all(compilers.map(this.checkCompiler));
+        const resolved = await Promise.all(
+            CompilerDefaults.map(this.checkCompiler),
+        );
         return resolved.filter((compiler) => compiler !== null);
     };
 
-    private checkCompiler = async (compiler: string) => {
-        const { code, output } = await spawn_process(["which", compiler]);
+    private checkCompiler = async ({
+        executable,
+        family,
+    }: CompilerDefaultType) => {
+        const { code, output } = await spawn_process(["which", executable]);
         if (code !== 0) {
             return null;
         }
         const realpath = await fs.realpath(output);
         if (!this.versionCache.has(realpath)) {
-            this.versionCache.set(realpath, this.getVersion(realpath));
+            this.versionCache.set(realpath, this.getVersion(realpath, family));
         }
         try {
             const version = await this.versionCache.get(realpath);
-            return { command: compiler, path: realpath, version };
+            return { command: executable, path: realpath, version };
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
         } catch (e) {
             return null;
         }
     };
 
-    private getVersion = async (executable: string) => {
+    private getVersion = async (executable: string, family: CompilerFamily) => {
         const { code: versionCode, output: version } = await spawn_process([
             executable,
-            "--version",
+            ...CompileVersionArgs[family],
         ]);
         if (versionCode !== 0) {
             console.error(
